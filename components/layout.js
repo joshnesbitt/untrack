3001f82a689151d3d8ba0184a48a5cfa385857d4
Josh Nesbitt <josh@stac.works>
2021-03-04 15:59:20 +0000

    Add social tag.

--- components/layout.js
+++ components/layout.js
@@ -4,13 +4,30 @@ import Header from './header';
 import Footer from './footer';
 import Styles from './styles';
 
+import Config from '../config';
+
 export default function Layout({ children }) {
   return (
     <div>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <title>Untrack</title>
+
+        <meta content="website" property="og:type" />
+        <meta content={Config.title} property="og:title" />
+        <meta content={Config.description} property="og:description" />
+        <meta content={Config.url} property="og:url" />
+        <meta content={Config.image} property="og:image" />
+
+        <meta content="summary" name="twitter:card" />
+        <meta content={Config.title} name="twitter:title" />
+        <meta content={Config.description} name="twitter:description" />
+        <meta content={Config.url} name="twitter:url" />
+        <meta content={`@${Config.twitter.username}`} name="twitter:site" />
+        <meta content={`@${Config.twitter.username}`} name="twitter:creator" />
+        <meta content={Config.image} name="twitter:image" />
+
+        <title>{Config.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Untrack',
+  description: 'A minimal app to remove tracking data from any link.',
+  url: 'https://untrack.link',
+  image: 'https://untrack.link/social.jpg',
+  twitter: {
+    username: 'joshnesbitt'
+  }
+};
