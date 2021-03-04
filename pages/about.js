1df215dc1657bd7cdc6f652bb6976d3c7c8e94b0
Josh Nesbitt <josh@stac.works>
2021-03-04 17:22:05 +0000

    Fix typo.

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ export default function Index() {
           Why Build This?
         </h1>
 
-        <p>These days, most websites and apps track data onto the end of any URL. As a result of this, websites can track and fingerprint users across the web. This situation is compounded when family or friends share the same URL, allowing companies to detail a complex profile of you and your social circle's interests.</p>
+        <p>These days, most websites and apps tack data onto the end of any URL. As a result of this, websites can track and fingerprint users across the web. This situation is compounded when family or friends share the same URL, allowing companies to detail a complex profile of you and your social circle's interests.</p>
         <p>We can do our best to reduce our use of these sorts of services but doing this every day can be time-consuming and inconvenient. A more significant solution to this problem is founded in a deeper migration away from these sorts of websites, but getting your entire network of friends and family to do the same can be tricky, to say the least.</p>
         <p>Simple utilities like Untrack give the user more control over how they share information on the web. By stripping tracking information from any URL, it's a step in the right direction to living in a more privacy-aware world.</p>
       </main>
